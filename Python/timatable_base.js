d31c4a60c6c582075f0bd397e7cbf082f1336790
Jakob Woittennek <jakob@woittennek.net>
2026-07-23 11:49:21 +0200

    add some selction to timetable, but not fully implemented with js yet

--- Python/timatable_base.js
+++ Python/timatable_base.js
@@ -1,6 +1,67 @@
 function getInfo(element) {
 
 }
+async function init() {
+    let weekSelect = document.getElementById('week-select');
+}
+
+async function typeChanged(selectElement) {
+    let selectedType = selectElement.value;
+    let elementSelect = document.getElementById('element-select');
+    elementSelect.innerHTML = '';
+    if (selectedType === '0') {
+        // Class
+        let classes = await fetch('classes');
+        classes = await classes.text();
+        classes = JSON.parse(classes);
+        for (let cls of classes) {
+            let option = document.createElement('option');
+            option.value = cls.id;
+            option.text = cls.short_name + ' (' + cls.long_name + ')';
+            elementSelect.appendChild(option);
+        }
+    } else if (selectedType === '1') {
+        // Teacher
+        let teachers = await fetch('teachers');
+        teachers = await teachers.text();
+        teachers = JSON.parse(teachers);
+        for (let teacher of teachers) {
+            let option = document.createElement('option');
+            option.value = teacher.id;
+            option.text = '(' + teacher.short_name + ') ' + teacher.long_name;
+            elementSelect.appendChild(option);
+        }
+    } else if (selectedType === '2') {
+        // Room
+        let rooms = await fetch('rooms');
+        rooms = await rooms.text();
+        rooms = JSON.parse(rooms);
+        for (let room of rooms) {
+            let option = document.createElement('option');
+            option.value = room.id;
+            option.text = room.short_name + ' (' + room.long_name + ')';
+            elementSelect.appendChild(option);
+        }
+    }
+    updateTimetable();
+}
+
+async function elementChanged(selectElement) {
+    updateTimetable();
+}
+
+async function weekChanged(selectElement) {
+    updateTimetable();
+}
+
+async function updateTimetable() {
+    let typeSelect = document.getElementById('type-select');
+    let elementSelect = document.getElementById('element-select');
+    let selectedType = typeSelect.value;
+    let selectedElement = elementSelect.value;
+    console.log("Selected Type: " + selectedType);
+    console.log("Selected Element: " + selectedElement);
+}
 async function parseTimeTable() {
     let weekdays = ['mo', 'tu', 'we', 'th', 'fr'];
     let data = await fetch('timetable2.json')
